1f3ea336347df550944d1bbb8d85ff05b84c4162
cube <cube>
2004-02-24 22:11:28 +0000

    Initial import of gkrellm, version 2.1.25, into the NetBSD Package Collection.  This is the GTK+2 port of old gkrellm1.
    Package imported from pkgsrc-wip.
    
    With a single process, GKrellM manages multiple stacked monitors and supports
    applying themes to match the monitors appearance to your window manager,
    Gtk, or any other theme.
    
      * SMP CPU, Disk, Proc, and active net interface monitors with LEDs.
      * Internet monitor that displays current and charts historical port hits
    
      * Memory and swap space usage meters and a system uptime monitor.
      * File system meters show capacity/free space and can mount/umount.
      * A mailbox monitor which can launch mail reader, remote mail fetch.
      * Clock/calendar and hostname display.
      * APM laptop battery monitor.
      * CPU/motherboard temperature display if lm_sensors modules installed.
    
      * Multiple monitors managed by a single process to reduce system load.
      * A timer button that can execute PPP or ISDN logon/logoff scripts.
      * Charts are autoscaling with configurable grid line resolution, or
        can be set to a fixed scale mode.
      * Separate colors for "in" and "out" data.  The in color is used for
        CPU user time, disk read, forks, and net receive data.  The out color
        is used for CPU sys time, disk write, load, and net transmit data.
      * Commands can be configured to run when monitor labels are clicked.
      * GKrellM is plugin capable so special interest monitors can be created.
      * A different theme can be created with the GIMP.
    
    While GKrellM monitors the host it is running on, it can poll a server
    over the network.  The server package is named gkrellm-server.

---
+++ sysutils/gkrellm/buildlink2.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/02/24 22:11:28 cube Exp $
+
+.if !defined(GKRELLM_BUILDLINK2_MK)
+GKRELLM_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			gkrellm
+BUILDLINK_DEPENDS.gkrellm?=		gkrellm>=2.1.22
+BUILDLINK_PKGSRCDIR.gkrellm?=		../../sysutils/gkrellm
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gkrellm=gkrellm
+BUILDLINK_PREFIX.gkrellm_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.gkrellm+=	include/gkrellm2/gkrellm.h
+BUILDLINK_FILES.gkrellm+=	include/gkrellm2/gkrellm-public-proto.h
+
+PTHREAD_OPTS+=		require
+
+.include "../../x11/gtk2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	gkrellm-buildlink
+
+gkrellm-buildlink: _BUILDLINK_USE
+
+.endif	# GKRELLM2_BUILDLINK2_MK

d9a9f44ad1f65febfef502dc3711ae7a82dce7a9
jlam <jlam>
2004-03-29 05:05:32 +0000

    Add a BUILDLINK_PKGBASE.<pkg> definition where it's not equal to <pkg>, e.g. "BUILDLINK_PKGBASE.gtk?= gtk+".  This is mandated by the example buildlink[23].mk files in bsd.buildlink[23].mk.

--- net/py-METAR/buildlink2.mk
+++ net/py-METAR/buildlink2.mk
@@ -1,7 +1,8 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2002/12/03 19:34:59 drochner Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/03/29 05:05:42 jlam Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=		pymetar
-BUILDLINK_DEPENDS.pymetar?=	${PYPKGPREFIX}-pymetar-*
+BUILDLINK_PKGBASE.pymetar?=	${PYPKGPREFIX}-pymetar
+BUILDLINK_DEPENDS.pymetar?=	${PYPKGPREFIX}-pymetar-[0-9]*
 BUILDLINK_PKGSRCDIR.pymetar?=	../../net/py-METAR

16a4eed5eac9b2aaa6e2814b440e777d68c502cb
recht <recht>
2004-05-23 11:58:21 +0000

    unused

--- net/py-METAR/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.2 2004/03/29 05:05:42 jlam Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pymetar
-BUILDLINK_PKGBASE.pymetar?=	${PYPKGPREFIX}-pymetar
-BUILDLINK_DEPENDS.pymetar?=	${PYPKGPREFIX}-pymetar-[0-9]*
-BUILDLINK_PKGSRCDIR.pymetar?=	../../net/py-METAR

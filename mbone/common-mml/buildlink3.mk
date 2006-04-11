f7632caf03f7ead8a469341442fbb3bc035e5461
minskim <minskim>
2006-04-11 17:57:08 +0000

    Import common-mml from pkgsrc-wip.  Packaged by David Price.
    The UCL common multimedia library implements a number of algorithms and
    protocols needed by a number of our applications.
    
    The following protocols/algorithms are included in the library:
    
     * Base64 encoding/decoding
     * Binary tree
     * Random number
     * HMAC authentication
     * MD5
     * DES
     * RTP
     * Mbus
     * SAP
     * SDP

---
+++ mbone/common-mml/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/04/11 17:57:08 minskim Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+COMMON_MML_BUILDLINK3_MK:=	${COMMON_MML_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	common-mml
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncommon-mml}
+BUILDLINK_PACKAGES+=	common-mml
+
+.if !empty(COMMON_MML_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.common-mml+=	common-mml>=1.2.14
+BUILDLINK_PKGSRCDIR.common-mml?=	../../mbone/common-mml
+.endif	# COMMON_MML_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}

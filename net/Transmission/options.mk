ba6f7fad0c90d7352e61353867c2cff523aae786
tnn <tnn>
2007-10-30 22:31:57 +0000

    Update to Transmission-0.91. Note that the distfile is slightly borked and extracts with warnings: tar: Invalid header, starting valid header search. This does not seem to affect the package, however. OpenSSL is now mandatory so the ssl pkg option was removed.
    News:
    0.91 (2007/10/28)
    - libtransmission (all platforms):
       + Fix 0.90 speed limits
       + Fix 0.90 problems announcing to some trackers
       + Fix 0.90 socket connection leak
       + Fix 0.90 IPC crash
       + Fix 0.90 cache bug that could cause "verify local files" to fail
       + Fix 0.90 build errors on OpenBSD and on older C compilers
    - GTK:
       + Fix 0.90 packaging errors
       + Fix 0.90 crash-on-start with assertion failure: "destination != (void*)0"
    
    0.90 (2007/10/23)
     - Encryption support, with option to ignore unencrypted peers
     - Ban peers that send too much bad data
     - Only report downloaded, verified good pieces in tracker `download' field
     - Improved compliance with BitTorrent spec
     - MSE Tracker Extension support
     - Significant rewrite of the libtransmission back-end
     - Icon enhanced to support size of 512 x 512
     - GTK:
       + Various interface improvements
       + Better compliance with the Gnome interface guidelines

--- net/Transmission/options.mk
+++ net/Transmission/options.mk
@@ -1,25 +1,33 @@
-# $NetBSD: options.mk,v 1.1 2007/08/03 22:34:23 tnn Exp $
+# $NetBSD: options.mk,v 1.2 2007/10/30 22:31:58 tnn Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.Transmission
-PKG_SUPPORTED_OPTIONS=	gtk ssl
-PKG_SUGGESTED_OPTIONS=	gtk ssl
+PKG_SUPPORTED_OPTIONS=	gtk # wxwidgets # wx currently doesn't build.
+PKG_SUGGESTED_OPTIONS=	gtk
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mssl)
-.	include "../../security/openssl/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-openssl
-.endif
-
 .if !empty(PKG_OPTIONS:Mgtk)
-.	include "../../x11/gtk2/buildlink3.mk"
-USE_TOOLS+=		msgfmt pkg-config
+. include "../../x11/gtk2/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-gtk
+USE_TOOLS+=		intltool msgfmt
 USE_DIRS+=		xdg-1.1
 PLIST_SUBST+=		GTK=
 .else
-CONFIGURE_ARGS+=        --disable-gtk
+CONFIGURE_ARGS+=        --without-gtk
 PLIST_SUBST+=		GTK="@comment "
+pre-configure:
+	touch ${WRKSRC}/po/Makefile
+.endif
+
+.if !empty(PKG_OPTIONS:Mwxwidgets)
+. include "../../x11/wxGTK/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-wx
+USE_LANGUAGES+=		c c++
+. if empty(PKG_OPTIONS:Mgtk)
+PKG_FAIL_REASON+=	"The wxwidgets option needs the gtk option."
+. endif
+.else
+CONFIGURE_ARGS+=	--without-wx
 .endif

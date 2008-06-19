169e7951d591a343113630e22b51bc0ac044626b
tnn <tnn>
2008-06-19 08:15:48 +0000

    Update to Transmission-1.22. From Matthias Drochner. All Platforms + Fix two separate BitTorrent unchoke bugs that could affect performance + Transmission now builds correctly on Sun Studio + Minor man page fixes GTK+ + Fix crash when quitting while the stats window is still up + Added Latvian, Malayalam, Serbian, and Telugu translations + Updated Czech, Spanish, Romanian, Russian, Dutch, Polish, Italian, Portuguese, Catalan, Danish, German, Swedish, Traditional Chinese, Finnish, and Chinese (simplified) translations CLI: + Fix scraping torrents with the -s command-line argument

--- net/Transmission/options.mk
+++ net/Transmission/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2008/02/18 13:19:15 tnn Exp $
+# $NetBSD: options.mk,v 1.7 2008/06/19 08:15:48 tnn Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -9,8 +9,9 @@ PKG_SUGGESTED_OPTIONS=	gtk
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgtk)
-. include "../../x11/gtk2/buildlink3.mk"
 . include "../../graphics/hicolor-icon-theme/buildlink3.mk"
+. include "../../sysutils/libnotify/buildlink3.mk"
+. include "../../x11/gtk2/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-gtk
 USE_DIRS+=		xdg-1.1
 PLIST_SRC+=		${PKGDIR}/PLIST.gtk

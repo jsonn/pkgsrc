c1028588ec60fc05d58407ac4ccbe79e83a59321
tnn <tnn>
2009-05-15 09:43:55 +0000

    Update to Transmission-1.61. - Add PKG_OPTION "qt" (experimental)
    1.61:
    All Platforms
     * Close potential CSRF security hole for Web Client users
     * Fix locale error in the JSON parser
    Qt
     * Various small bugfixes to the beta Qt client
    Daemon
     * transmission-remote was unable to select torrents by their SHA1 hash
    Web Client
     * Fix 1.60 error when serving Web Client files on some embedded
     * Add response header to allow clients to cache static files
    
    1.60:
    All Platforms
     * Seed ratio limiting
     * Transfer prioritization
     * Option to specify if transfers are included in the global bandwidth limits
     * Random port, with optional randomization on startup
     * Improvements to UPnP port forwarding
     * Support for file preallocation on XFS filesystems
    Qt
     * New beta Qt client!
    GTK+
     * Speed Limit: Second set of bandwidth limits that can be toggled or scheduled
     * Properties dialog now lets you edit/view multiple torrents at once
     * Allow sorting of the torrent list by size and by ETA
     * Show the file icon in the list
    Daemon
     * Watch folder for auto-adding torrents
     * Many new features in the RPC/JSON interface
     * Allow users to specify a specific address when listening for peers
    Web Client
     * File selection and prioritization
     * Add option to verify local data
     * Fix "Remove Data" bug

--- net/Transmission/options.mk
+++ net/Transmission/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.11 2009/02/16 22:29:49 tnn Exp $
+# $NetBSD: options.mk,v 1.12 2009/05/15 09:43:55 tnn Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.Transmission
-PKG_SUPPORTED_OPTIONS=	gtk
+PKG_SUPPORTED_OPTIONS=	gtk qt
 PKG_SUGGESTED_OPTIONS=	gtk
 
 .include "../../mk/bsd.options.mk"
@@ -20,4 +20,24 @@ PLIST_SRC+=		${PKGDIR}/PLIST.gtk
 CONFIGURE_ARGS+=	--without-gtk
 pre-configure:
 	touch ${WRKSRC}/po/Makefile
+.endif
+
+.if !empty(PKG_OPTIONS:Mqt)
+. if empty(PKG_OPTIONS:Mgtk)
+PKG_FAIL_REASON=	"qt needs gtk option (for now)"
+. endif
+USE_LANGUAGES+=		c c++
+PLIST_SRC+=		${PKGDIR}/PLIST.qt
+MAKE_ENV+=		QTDIR=${QTDIR}
+INSTALL_ENV+=		INSTALL_ROOT=${DESTDIR}${PREFIX}
+. include "../../x11/qt4-tools/buildlink3.mk"
+.PHONY:		build-qt-client
+post-build:	build-qt-client
+build-qt-client:
+	cd ${WRKSRC}/qt && ${SETENV} ${MAKE_ENV} ${QTDIR}/bin/qmake qtr.pro
+	cd ${WRKSRC}/qt && ${SETENV} ${MAKE_ENV} make
+.PHONY:		install-qt-client
+post-install:	install-qt-client
+install-qt-client:
+	cd ${WRKSRC}/qt && ${SETENV} ${INSTALL_ENV} make install
 .endif
